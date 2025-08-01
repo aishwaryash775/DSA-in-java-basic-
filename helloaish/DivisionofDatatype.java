b1c31334acc380d8762ebc61cb9d6e8f154b0bcf
aishwaryash775 <aishwaryash880@gmail.com>
2025-08-01 17:33:36 +0530

    Add files via upload

---
+++ helloaish/DivisionofDatatype.java
@@ -0,0 +1,17 @@
+package helloaish;
+
+import java.util.Scanner;
+
+public class DivisionofDatatype {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x = 5.0/2;
+        System.out.println(x);
+        //double/int note= always use 1.0 if we want value in double
+        int p = 68 , r =55, t = 44;
+        double e = p*r*t/100.0;
+        System.out.println(e);
+    }
+
+}
+
---
+++ helloaish/chardatatype.java
@@ -0,0 +1,27 @@
+package helloaish;
+
+import java.sql.SQLOutput;
+
+public class chardatatype {
+    public static void main(String[] args) {
+//        //typecasting - ek data type se doosra data type conversion
+//        char aish = 'A';
+//        int x = aish ; //implicit typecasting
+//        System.out.println(x);
+//
+//        char hell = 'a'; //explicit typecasting
+//        int y = (int)hell;
+//        System.out.println(y);
+//
+//        char mom = '7';
+//        System.out.println((int)mom);//explicit
+//
+//        char ch = 'c';
+//        System.out.println(ch*ch);
+//
+//        // integer to character
+//        int x = 17;
+//        char ch =(char)x;
+//        System.out.println(ch);
+    }
+}
