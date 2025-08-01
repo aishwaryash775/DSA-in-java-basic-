ad3d4443c228942636399d06934cf481079fe561
aishwaryash775 <aishwaryash880@gmail.com>
2025-08-01 17:31:54 +0530

    Add files via upload

---
+++ helloaish/incrementdecrement.java
@@ -0,0 +1,10 @@
+package helloaish;
+
+public class incrementdecrement {
+    public static void main(String[] args) {
+        int x = 10;
+        x--; // x=x-1;
+        System.out.println(x);
+
+    }
+}
